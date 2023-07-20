881e410b6f1602c3e31beaa40e06d789080fa28b
jinseoit <jinseoit@gmail.com>
2023-07-20 11:37:04 +0900

    [ADD] BOJ 노드사이의 거리 추가

--- baekjoon/test.js
+++ baekjoon/test.js
@@ -1,25 +1,32 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
+const [n, m] = input[0].split(" ").map(Number);
+// 인접 리스트
+const graph = []
 
-let n = Number(input[0]); // 정점의 개수(N)
-let m = Number(input[1]); // 간선의 개수(M)
-let graph = [];
-for(let i = 1; i<=n; i++) graph[i] = [];
-for(let i = 2; i<=m+1; i++) {
-    let [x,y] = input[i].split(' ').map(Number);
-    graph[x].push(y);
-    graph[y].push(x);
+for(let i =1; i<=n;i++) graph[i] = [];
+for(let i =1; i<n;i++){
+    const [a,b,cost] = input[i].split(" ").map(Number);
+    graph[a].push([b, cost])
+    graph[b].push([a, cost])
 }
 
-let cnt = 0;
-let visited = new Array(n+1).fill(false);
-function dfs(x){
-    visited[x] = true;
-    cnt++;
-    for(y of graph[x]) {
-        if(!visited[y]) dfs(y)
-    }
+let visited;
+let distance;
+
+function dfs(x, dist){
+    if(visited[x]) return;
+    visited[x] = true // 각 노드는 한번만 방문;
+    distance[x] = dist;
+    for(let [y, cost] of graph[x]) dfs(y, dist+cost)
 }
-dfs(1);
-console.log(cnt - 1);
+
+for(let i=0; i<m; i++){
+    let [x,y] = input[n+i].split(' ').map(Number);
+    visited = new Array(n+i).fill(false);
+    distance = new Array(n+i).fill(-1);
+    dfs(x, 0);    
+    console.log(distance[y]);
+}
+
