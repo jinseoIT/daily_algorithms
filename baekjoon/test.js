f1c308d4d89bc486f759772d910b72e68f42e7b8
jinseoit <jinseoit@gmail.com>
2023-07-25 23:54:14 +0900

    [ADD] 소수 만들기 추가

--- baekjoon/test.js
+++ baekjoon/test.js
@@ -2,56 +2,47 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-const [s, t] = input[0].split(' ').map(Number);
-
-if(s==t) {console.log(0); return;}
-
 class Queue {
-	constructor() {
-		this.items = {};
-		this.headIndex = 0;
-		this.tailIndex = 0; 
-	}
-	enqueue(x){
-		this.items[this.tailIndex] = x;
-		this.tailIndex++;
-	}
-	dequeue(){
-		const item = this.items[this.headIndex];
-		delete this.items[this.headIndex];
-		this.headIndex++;
-		return item;
-	}
-	peek(){
-		return this.items[this.headIndex];
-	}
-	getLength(){
-		return this.tailIndex - this.headIndex;
-	}
+  constructor(){
+      this.items = {};
+      this.headIndex = 0;
+      this.tailIndex = 0;
+  }
+  enqueue(item){
+      this.items[this.tailIndex] = item;
+      this.tailIndex++;
+  }
+  dequeue(){
+      const item = this.items[this.headIndex];
+      delete this.items[this.headIndex];
+      this.headIndex++;
+      return item; 
+  }
+  isCheck(item){
+      if(this.items[this.headIndex] == item) {
+          return true;
+      }    
+      return false;
+  }
 }
-
-const bfs = s => {
-	const queue = new Queue()
-	queue.enqueue({val :s, opers : ''});
-	let visited = new Set([s])
-	while(queue.getLength() != 0 ){
-		const {val, opers} = queue.dequeue(); 
-		if(val > 1e9) continue; 
-		if(val == t) return opers;
-    for(let operator of ['*', '+', '-', '/']){
-      let nextVal = val;
-      if(operator === '*') nextVal *= val;
-      if(operator === '+') nextVal += val;
-      if(operator === '-') nextVal -= val;
-      if(operator === '/') nextVal /= val;
-
-      if(!visited.has(nextVal)){
-        queue.enqueue({val: nextVal, opers: opers+operator})
-        visited.add(nextVal);
-		  }
-    }
-	}
+function solution(cards1, cards2, goal) {
+  let answer = true;
+  const card1Queue = new Queue();
+  const card2Queue = new Queue();
+  cards1.forEach(word => card1Queue.enqueue(word));
+  cards2.forEach(word => card2Queue.enqueue(word));
+  
+  for(let i = 0; i<goal.length; i++){
+      const word = goal[i];
+      if(card1Queue.isCheck(word)) {
+          card1Queue.dequeue();
+          continue;
+      } 
+      else if(card2Queue.isCheck(word)){
+          card2Queue.dequeue();
+          continue;
+      }
+      answer = false;
+  }
+  return answer ? 'Yes' : 'No';
 }
-
-const answer = bfs(s);
-console.log(answer ? answer : -1)
