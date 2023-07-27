326b7df6b63c4a43968b4a36f14531717ba4d1c3
jinseoit <jinseoit@gmail.com>
2023-07-27 16:05:05 +0900

    [ADD] 경쟁적 전역 추가

--- baekjoon/test.js
+++ baekjoon/test.js
@@ -2,10 +2,6 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-const [s, t] = input[0].split(' ').map(Number);
-
-if(s==t) {console.log(0); return;}
-
 class Queue {
 	constructor() {
 		this.items = {};
@@ -29,29 +25,60 @@ class Queue {
 		return this.tailIndex - this.headIndex;
 	}
 }
+// 작은수부터의 좌표를 알고 있어야 할 것 같다.
 
-const bfs = s => {
-	const queue = new Queue()
-	queue.enqueue({val :s, opers : ''});
-	let visited = new Set([s])
-	while(queue.getLength() != 0 ){
-		const {val, opers} = queue.dequeue(); 
-		if(val > 1e9) continue; 
-		if(val == t) return opers;
-    for(let operator of ['*', '+', '-', '/']){
-      let nextVal = val;
-      if(operator === '*') nextVal *= val;
-      if(operator === '+') nextVal += val;
-      if(operator === '-') nextVal -= val;
-      if(operator === '/') nextVal /= val;
+const [n, k] = input[0].split(' ').map(Number);
+let graph = []; // 전체 보드 정보 담는 리스트;
+let data = []; // 바이러스 대한 정보 리스트;
 
-      if(!visited.has(nextVal)){
-        queue.enqueue({val: nextVal, opers: opers+operator})
-        visited.add(nextVal);
-		  }
-    }
+for(let i = 0; i< n; i++){
+	graph.push(input[i+1].split(' ').map(Number));
+	for(let j = 0; j<n; j++){
+		// 해당 위치에 바이러스가 존재하는 경우
+		if(graph[i][j] != 0){
+			// 바이러스 종류 , 시간, 위치X , 위치Y 삽입
+			data.push([graph[i][j], 0, i ,j])
+		}
 	}
 }
 
-const answer = bfs(s);
-console.log(answer ? answer : -1)
+// 정렬 이후에 큐로 옮기기(낮은 번호의 바이러스가 먼저 증식)
+data.sort((a,b) => a[0]-b[0]);
+const queue = new Queue();
+for(let x of data){
+	queue.enqueue(x);
+}
+
+let [targetS, targetX, targetY] = input[n + 1].split(' ').map(Number);
+
+// 바이러스가 퍼져나갈수 있는 4가지 위치
+let dx = [-1,0,1,0];
+let dy = [0,1,0,-1];
+
+// 너비 우선 탐색(BFS) 진행
+while(queue.getLength() > 0){	
+	let [virus, s , x, y] = queue.dequeue();
+	// s초가 지나거나, 큐가 빌때 까지 반복
+	if(s == targetS) break;
+	for(let i = 0; i < 4; i++){
+		let nx = x + dx[i];
+		let ny = y + dy[i];
+		// 해당 위치로 이동할 수 있는 경우
+		if(nx >= 0 && nx < n && ny >= 0 && ny < n){
+		// 아직 방문하지 않은 위치라면, 그 위치에 바이러스 넣기
+		if(graph[nx][ny] == 0){
+			graph[nx][ny] = virus;
+			queue.enqueue([virus, s+1, nx ,ny]);
+		}
+		}
+	}
+}
+console.log(graph[targetX - 1][targetY -1]);
+
+
+
+
+
+
+
+
