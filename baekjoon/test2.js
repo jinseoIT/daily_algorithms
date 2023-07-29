8d2eca9ad3c911508d5a53a2882f98660379d748
jinseoit <jinseoit@gmail.com>
2023-07-29 15:14:26 +0900

    [ADD] 키패드 누르기 추가

--- baekjoon/test2.js
+++ baekjoon/test2.js
@@ -2,59 +2,22 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-class Queue{
-	constructor(){
-		this.items = {};
-		this.headIndex = 0;
-		this.tailIndex = 0;
-	}
-	enqueue(item){ //넣기 
-		this.items[this.tailIndex] = item;
-		this.tailIndex++;
-	}
-	dequeue(){ // 빼기
-		const item = this.items[this.headIndex];
-		delete this.items[this.headIndex];
-		this.headIndex++;
-		return item;
-	} 
-	peek(){
-		return this.items[this.headIndex]
-	}
-	getLength(){
-		return this.tailIndex - this.headIndex;
-	} 
-}
-
-let [n, m ,k, x] = input[0].split(' ').map(Number); 
-let graph = [];
-for(let i =1; i<=n; i++){graph[i] = []}
+const n = Number(input[0]);
+let words = [];
 
 for(let i = 1; i<=n; i++){
-    let [x,y] = input[i].split(' ').map(Number);
-    graph[x].push(y);
+	words.push(input[i]);
 }
-// 모든 도시에 대한 최단 거리 초기화
-let distance = new Array(1+n).fill(-1);
-distance[x] = 0;
+words = new Set(words);
+words = [...words].sort((a,b) => {
+	if(a.length < b.length) return -1;
+	if(b.length > a.length) return 1;
+	if(a.length == b.length){
+		return a < b ? -1 : 1;
+	};
+	return 0;
+})
 
-let queue = new Queue();
-queue.enqueue(x);
-while(queue.getLength() > 0) {
-    let now = queue.dequeue();
-    for(let nextNode of graph[now]){
-        if(distance[nextNode] == -1){
-            distance[nextNode] == distance[now] + 1;
-            queue.enqueue(nextNode);
-        }
-    }
-}
-
-let check = false;
-for(let i = 1; i <=n; i++){
-    if(distance[i] == k){
-        console.log(i);
-        check = true;
-    }
-}
-if(!check) console.log(-1)
+words.forEach(v=> {
+	console.log(v);
+})
