09e58e8299423b9e6df441fb8ab0def2f25ba6ad
jinseoit <jinseoit@gmail.com>
2023-08-16 15:15:18 +0900

    [ADD] 백준 치즈 추가

--- baekjoon/test.js
+++ baekjoon/test.js
@@ -14,7 +14,7 @@ class Queue {
 	}
 	dequeue() {
 		const item = this.items[this.headIndex];
-		this.headIndex++;
+		this.headIndex++; 
 		return item;
 	}
 	getLength(){
@@ -22,33 +22,62 @@ class Queue {
 	}
 }
 
-// n : 동기수들, 친구 관계의 수 : m 
-// [친구]와 [친구의 친구]까지 결혼식에 초대 하자
-const n = +input[0];
-const m = +input[1];
-
-const graph = Array.from({length: n+1}, () => []);
-const invited = new Array(n+1).fill(0);
-
-for(let i = 2; i<m+2; i++){
-	const [a,b] = input[i].split(' ').map(Number);
-	graph[a].push(b);
-	graph[b].push(a);
+// 맵의 크기 n, m 저보 입력
+let [n, m] = input[0].split(' ').map(Number);
+let graph = []; // 2차원 맵 입력 받기
+for(let i = 1; i <= n; i++) {
+	let row = input[i].split(' ').map(Number);
+	graph.push(row);
 }
+// 상, 하 , 좌, 우 방향 정보
+let dx = [-1, 1, 0, 0];
+let dy = [0, 0, -1, 1];
 
-const queue = new Queue();
-queue.enqueue(1);
-
-while(queue.getLength() > 0){
-	const target = queue.dequeue();
-	for(let person of graph[target]){
-		if(!invited[person]){
-			invited[person] = invited[target] + 1;
-			queue.enqueue(person);
+const bfs = () => {
+	let visited = [];  // 방문 처리 배열
+	for (let i = 0; i < n; i++) visited.push(new Array(m).fill(false));
+	visited[0][0] = true; // 제일 왼쪽 위에서 출발
+	let queue = new Queue(); // 너비 우선 탐색(BFS) 수행
+	queue.enqueue([0,0]);
+	while(queue.getLength() != 0){  // 큐가 빌 때까지 반복
+		let [x, y] = queue.dequeue();
+		for(let i = 0; i< 4; i++){ 
+			let nx = x + dx[i];
+			let ny = y + dy[i];
+			// 맵을 벗어나는 경우 무시
+			if (nx < 0 || nx >=n || ny< 0 || ny >= m) continue;
+			if(!visited[nx][ny]){
+				if(graph[nx][ny] >= 1) graph[nx][ny] +=1; // 카운트 증가
+				else {
+					queue.enqueue([nx, ny]);
+					visited[nx][ny] = true;
+				}
+			}
 		}
 	}
 }
-// (친구 수 + 친구의 친구 수) - 나자신
-const totalInvitedCnt = invited.filter(v=> !!v && v<=2).length - 1;
+const melt = () => {
+	let finish = true; // 더 녹일 치즈가 없는지 여부
+	for(let i = 0; i< n; i++){
+		for(let j = 0; j<m; j++){
+			if(graph[i][j] >= 3) { // 녹일 치즈라면
+				graph[i][j] = 0; // 녹이기
+				finish = false;
+			}
+			else if(graph[i][j] == 2) graph[i][j] = 1; // 한 면만 닿은 경우 무시
+		}
+	}
+	return finish
+}
 
-console.log(totalInvitedCnt > 0 ? totalInvitedCnt : 0);
+let result = 0;
+
+while(true){
+	bfs();
+	if(melt()){
+		console.log(result); // 전부 녹았다면
+		break;
+	}
+	else result += 1;
+}
+
