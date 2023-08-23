b5c0bf7457ec70b9de8ab6a6ec9108ba1628035f
jinseoit <jinseoit@gmail.com>
2023-08-23 14:32:27 +0900

    [ADD] 타일 추가

--- baekjoon/test.js
+++ baekjoon/test.js
@@ -3,17 +3,9 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const n = Number(input[0]);
-const dp = [[1,0],[0,1]];
+const dp = [0, 1, 2, 3];
 
-const getFibo = (num) => {
-    for(let i=2; i<=num;i++){
-        if(!dp[i]){
-            dp[i] = [dp[i-1][0]+dp[i-2][0], dp[i-1][1]+dp[i-2][1]]
-        }
-    }
-    console.log(`${dp[num][0]} ${dp[num][1]}`)
+for(let i = 3; i<=n; i++){ 
+    dp[i] = (dp[i-1] + dp[i-2]) % 15746;
 }
-
-for(let i = 1; i<=n; i++){
-    getFibo(input[i])
-}  
+console.log(dp[n]);
