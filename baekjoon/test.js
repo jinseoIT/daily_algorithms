879c29059d87316f1a84a19c9a2c185a5bb35ce6
jinseoit <jinseoit@gmail.com>
2023-09-19 23:29:10 +0900

    [ADD] 쉬운 계단 수 추가

--- baekjoon/test.js
+++ baekjoon/test.js
@@ -2,34 +2,26 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-// 우선순위 먼저
-// 우선순위가 같다면 Queue의 가장 뒤에 재배치 한다
-function solution(priorities, location){
-    let answer = 0;
-    let queue = priorities;
-    let indexArr = Array.from({length: queue.length}, (v,i) => i);
-    indexArr[location] = 'target';
-
-    while(1){
-        if(queue[0] === Math.max(...queue)){
-            answer +=1;
-            if(indexArr[0] === 'target'){
-                break;
-            } else {
-                queue.shift();
-                indexArr.shift();
-            }
-        } else {
-            queue.push(queue.shift());
-            indexArr.push(indexArr.shift());
-        }
-    }
-    return answer;
-}
-
 let n = Number(input[0]);
-for(let i = 1; i<= n*2; i+=2){
-    const [num, target] = input[i].split(' ').map(Number);
-    const priorities = input[i+1].split(' ').map(Number);
-    console.log(solution(priorities, target))
+let dp = Array.from(Array(n+1), () => new Array(10).fill(0));
+
+dp[1][0] = 0; // 0으로 시작하는 수는 계단수가 아님
+for(let j = 1; j <= 9; j++) {
+    dp[1][j] = 1;
 }
+
+for (let i = 2; i <= n; i++) { // 점화식에 따라서 DP 테이블 채우기 
+    for (let j = 0; j <= 9; j++) {
+        dp[i][j] = 0; 
+        if(j > 0) dp[i][j] += dp[i-1][j-1];
+        if(j < 9) dp[i][j] += dp[i-1][j+1];
+        dp[i][j] %= Number(1e9);
+    }
+}
+
+let result = 0;
+for (let j = 0; j <= 9; j++) {
+    result += dp[n][j];
+    result %= Number(1e9);
+}
+console.log(result);
