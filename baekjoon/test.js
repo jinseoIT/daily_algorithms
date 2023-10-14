cf79b8d630d468bcb25781a5f9cd8b83fc3151b1
jinseoit <jinseoit@gmail.com>
2023-10-15 01:59:06 +0900

    [ADD] 연결 요소의 개수 - dfs 추가

--- baekjoon/test.js
+++ baekjoon/test.js
@@ -1,82 +1,31 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
+// n 정점 m 간선
+const [n, m] = input[0].split(' ').map(Number);
 
-class Queue {
-    constructor() {
-        this.items = {};
-        this.headIndex = 0;
-        this.tailIndex = 0;
-    }
-    enqueue(item) {
-      this.items[this.tailIndex] = item;
-      this.tailIndex++;
-    }
-    dequeue() {
-      const item = this.items[this.headIndex];
-      delete this.items[this.headIndex];
-      this.headIndex++;
-      return item;
-    }
-    peek(){
-      return this.items[this.headIndex];
-    }
-    getLength(){
-      return this.tailIndex - this.headIndex;
-    }
+const visited = Array(n+1).fill(false);
+const graph = Array.from({length:n+1}, () => [])
+
+for(let i = 1; i<=m; i++){
+  const [a, b] = input[i].split(' ');
+  graph[a].push(b);
+  graph[b].push(a);
 }
-let n = Number(input[0]); // row의 개수
-// 우 하 좌 상
-const position = [[1, 0], [0, 1], [-1, 0], [0, -1]] 
 
-let graph = [];
-for(let i = 1; i <=n; i++){
-  graph.push(input[i].split(""));
-}
-const isRangeOver = (x, y) => {
-  if (x < 0 || x >= n || y < 0 || y >= n) return false;
-  return true;
-};
-// 방문 확인
-let visited = Array.from({length: n}, () => Array(n).fill(false));
-
-const bfs = (x, y) =>{
-  const queue = new Queue();
-  queue.enqueue([x,y]);
-  while(queue.getLength()) {
-    const [cx,cy] = queue.dequeue();
-    for([a,b] of position){
-      const [nx, ny] = [cx+a, cy+b];
-      if(isRangeOver(nx,ny) && !visited[nx][ny] && graph[cx][cy] == graph[nx][ny]) {
-        visited[nx][ny] = true;
-        queue.enqueue([nx,ny]);
-      }
-    }
+const dfs = (node) => {
+  if(visited[node]) return;
+  visited[node] = true;
+  for(target of graph[node]){
+    dfs(target);
   }
 }
 
-// 적록색약 아닌 경우
-let normalCnt = 0;
-for(let i = 0; i < n; i++){
-  for(let j = 0; j < n; j++){
-    if(!visited[i][j]){
-      bfs(i,j);
-      normalCnt++;
-    }
-  }
+let cnt = 0;
+for(let i = 1; i<=n; i++){
+  if(visited[i]) continue;
+  dfs(i);
+  cnt ++;
 }
 
-// 적록색약인 경우
-graph = graph.map((v) => v.map((l) => (l == "R" || l == "G" ? "RG" : l)));
-visited = Array.from({ length: n }, () => Array(n).fill(false));
-let RGWeaknessCnt = 0;
-for(let i = 0; i < n; i++){
-  for(let j = 0; j < n; j++){
-    if(!visited[i][j]){
-      bfs(i,j);
-      RGWeaknessCnt++;
-    }
-  }
-}
-
-console.log(`${normalCnt} ${RGWeaknessCnt}`);
+console.log(cnt);
