98c517fe1ad3dd3aca639a02a9c78c35c5a03f91
jinseoit <jinseoit@gmail.com>
2023-10-31 13:30:01 +0900

    [ADD] 카드2 추가

--- baekjoon/test.js
+++ baekjoon/test.js
@@ -1,51 +1,39 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
-let currentLine = 0;
-
-// 우 하 좌 상 대각선*4
-const position = [[1,0],[0,1],[-1,0],[0,-1],[1,1],[-1,1],[-1,-1],[1,-1]];
-
-const isOverRange = ([x,y],[h,w]) => {
-  if(x < 0 || y < 0 || x > h || y > w) return true;
-  return false
+const cardCount = Number(input[0]);
+class Queue {
+  constructor() {
+    this.items = {};
+    this.headIndex = 0;
+    this.tailIndex = 0;
+  }
+  enqueue(item){
+    this.items[this.tailIndex] = item;
+    this.tailIndex++;
+  }
+  dequeue(){
+    const item = this.items[this.headIndex];
+    delete this.items[this.headIndex];
+    this.headIndex++;
+    return item;
+  }
+  peek() {
+    return this.item[this.headIndex];
+  }
+  getLength() {
+    return this.tailIndex - this.headIndex;
+  }
 }
-
-while(true){
-  // w : 지도 넓이 , h : 지도 높이
-  const [mapW, mapH] = input[currentLine].split(' ').map(Number);
-  const [w, h] = [mapW - 1 , mapH -1];
-  currentLine++;
-
-  const map = [];
-  for(let i = 0; i<mapH; i++) {
-    const row = input[currentLine].split(' ').map(Number);
-    map.push(row);
-    currentLine++;
-  }
-
-  const visited = Array.from({length: mapH}, () => Array(mapW).fill(false));
-  const dfs = (x,y) => {
-    if(visited[x][y]) return; 
-    visited[x][y] = true;
-    if(!map[x][y]) return;
-    for([cx, cy] of position){
-      const [nx, ny] = [x+cx, y+cy];
-      if(!isOverRange([nx,ny],[h,w])){
-        dfs(nx,ny)
-      }
-    }
-  }
-
-  let cnt = 0;
-  for(let i=0; i<mapH; i++){
-    for(let j=0; j<mapW; j++){
-      if(visited[i][j] || !map[i][j]) continue;
-      dfs(i, j);
-      cnt ++;
-    }
-  }
-  
-  if(mapW == 0 && mapH == 0) break;
-  console.log(cnt);
+const cards = new Queue();
+for(let i = 1; i<=cardCount; i++){
+  cards.enqueue(i);
 }
+while(cards.getLength() > 1){
+  cards.dequeue();
+  const card = cards.dequeue();
+  cards.enqueue(card);
+}
+console.log(cards.dequeue());
+
+
