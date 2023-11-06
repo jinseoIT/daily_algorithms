51d72dc0e866f968328ab0e037fc402dbca153f6
jinseoit <jinseoit@gmail.com>
2023-11-07 03:09:16 +0900

    [ADD] 더하기 1,2,3 dp 추가

--- baekjoon/test.js
+++ baekjoon/test.js
@@ -1,13 +1,16 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
-const [N, M] = input[0].split(" ").map(Number);
-const passwordWithSite = {};
-for(let i = 1; i<=N; i++){
-  const [site, password] = input[i].split(" ");
-  passwordWithSite[site] = password;
+const T = Number(input[0]);
+
+const dp = [1,2,4]
+const getCount = (num) => {
+  for(let i =0; i<num;i++){
+    if(dp[i]) continue;
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+  }
+  return dp[num-1]
 }
-for(let i=N+1;i<=N+M; i++){
-  const site = input[i];
-  console.log(passwordWithSite[site]);
+for(let i =1; i<=T; i++){
+  console.log(getCount(Number(input[i])));
 }
