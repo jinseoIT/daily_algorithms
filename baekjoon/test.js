b013b6b975a038d35a4537ac5ba5aff718f05a57
jinseoit <jinseoit@gmail.com>
2023-11-09 23:49:24 +0900

    [ADD] 케빈베이컨 6단계 코드수정

--- baekjoon/test.js
+++ baekjoon/test.js
@@ -1,16 +1,65 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
-const T = Number(input[0]);
 
-const dp = [1,2,4]
-const getCount = (num) => {
-  for(let i =0; i<num;i++){
-    if(dp[i]) continue;
-    dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+class Queue {
+  constructor() {
+    this.items = {};
+    this.headIndex = 0;
+    this.tailIndex = 0;
   }
-  return dp[num-1]
+  enqueue(item) {
+    this.items[this.tailIndex] = item;
+    this.tailIndex++;
+  }
+  dequeue(){
+    const item = this.items[this.headIndex];
+    delete this.items[this.headIndex];
+    this.headIndex++;
+    return item;
+  }
+  peek(){
+		return this.items[this.headIndex]
+	}
+	getLength(){
+		return this.tailIndex - this.headIndex;
+	} 
 }
-for(let i =1; i<=T; i++){
-  console.log(getCount(Number(input[i])));
+// N: 유저의 수 , M: 친구 관계의 수
+const [N, M] = input[0].split(" ").map(Number);
+
+const graph = Array.from({length: N+1}, () => []);
+for(let i = 1; i<=M; i++){
+  const[A, B] = input[i].split(" ").map(Number);
+  graph[A].push(B)
+  graph[B].push(A)
 }
+
+const bfs = (node) => {
+  const queue = new Queue();
+  const visited = Array(N+1).fill(0);
+  queue.enqueue([node,0]);
+  while(queue.getLength()){
+    const [current, distance] = queue.dequeue();
+    for(const B of graph[current]){
+      if(!visited[B] && B !== node){
+        visited[B] = distance+1;
+        queue.enqueue([B, distance+1]);
+      } 
+    }
+  }
+  return visited.reduce((acc, curr) => acc+curr);
+}
+const score = {};
+for(let i = 1; i<=N; i++){
+  score[i] = bfs(i);
+}
+const answer = Object.entries(score).sort((a,b) => {
+  if(a[1] < b[1]) return -1;
+  if(a[1] > b[1]) return 1;
+  if(a[0] < b[0]) return -1;
+  if(a[0] > b[0]) return 1;
+  return 0
+})[0][0]
+
+console.log(answer);
